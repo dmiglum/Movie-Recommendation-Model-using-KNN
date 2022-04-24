66e43b866a86d394648e37c357643db5c464ccb2
dmiglum <103585746+dmiglum@users.noreply.github.com>
2022-04-24 11:44:38 -0500

    Add files via upload

---
+++ Movies Recommendation Data.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  3 17:59:10 2022
+
+@author: Dmitriy Glumov
+"""
+import pandas as pd
+from collections import Counter
+import math
+
+def knn(data, query, k, distance_fn, choice_fn):
+    neighbor_distances_and_indices = []
+    
+    # 3. For each example in the data
+    for index, example in enumerate(data):
+        # 3.1 Calculate the distance between the query example and the current
+        # example from the data.
+        distance = distance_fn(example[:-1], query)
+        
+        # 3.2 Add the distance and the index of the example to an ordered collection
+        neighbor_distances_and_indices.append((distance, index))
+    
+    # 4. Sort the ordered collection of distances and indices from
+    # smallest to largest (in ascending order) by the distances
+    sorted_neighbor_distances_and_indices = sorted(neighbor_distances_and_indices)
+    
+    # 5. Pick the first K entries from the sorted collection
+    k_nearest_distances_and_indices = sorted_neighbor_distances_and_indices[:k]
+    
+    # 6. Get the labels of the selected K entries
+    k_nearest_labels = [data[i][-1] for distance, i in k_nearest_distances_and_indices]
+
+    # 7. If regression (choice_fn = mean), return the average of the K labels
+    # 8. If classification (choice_fn = mode), return the mode of the K labels
+    return k_nearest_distances_and_indices , choice_fn(k_nearest_labels)
+
+def mean(labels):
+    return sum(labels) / len(labels)
+
+def mode(labels):
+    return Counter(labels).most_common(1)[0][0]
+
+def euclidean_distance(point1, point2):
+    sum_squared_distance = 0
+    for i in range(len(point1)):
+        sum_squared_distance += math.pow(point1[i] - point2[i], 2)
+    return math.sqrt(sum_squared_distance)
+
+def recommend_movies(movie_query, k_recommendations):
+    raw_movies_data = []
+    with open('movies_recommendation_data.csv', 'r') as md:
+        # Discard the first line (headings)
+        next(md)
+
+        # Read the data into memory
+        for line in md.readlines():
+            data_row = line.strip().split(',')
+            raw_movies_data.append(data_row)
+
+    # Prepare the data for use in the knn algorithm by picking
+    # the relevant columns and converting the numeric columns
+    # to numbers since they were read in as strings
+    movies_recommendation_data = []
+    for row in raw_movies_data:
+        data_row = list(map(float, row[2:]))
+        movies_recommendation_data.append(data_row)
+
+    # Use the KNN algorithm to get the 5 movies that are most
+    # similar to The Post.
+    recommendation_indices, _ = knn(
+        movies_recommendation_data, movie_query, k=k_recommendations,
+        distance_fn=euclidean_distance, choice_fn=lambda x: None
+    )
+
+    movie_recommendations = []
+    for _, index in recommendation_indices:
+        movie_recommendations.append(raw_movies_data[index])
+
+    return movie_recommendations
+
+the_post = [7.2, 1, 1, 0, 0, 0, 0, 1, 0] # feature vector for The Post
+recommended_movies = recommend_movies(movie_query=the_post, k_recommendations=5)
+
+for movie in recommended_movies:
+    print (movie[1])
+
+########################################
+
+
+###################################
+### Reading files without pandas
+raw_movies_data = []
+with open('movies_recommendation_data.csv', 'r') as md:
+    test_var = md.readlines()
+    del test_var[0]
+    
+for line in test_var:
+    data_row = line.strip().split(',')
+    raw_movies_data.append(data_row)
+    
+movies_recommendation_data = []
+for row in raw_movies_data:
+    data_row = list(map(float, row[2:]))
+    movies_recommendation_data.append(data_row)
+##########
+
